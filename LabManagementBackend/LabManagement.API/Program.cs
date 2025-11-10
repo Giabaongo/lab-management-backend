a11170b4895f69c8bd7d3763e459cc71d8bc2652
hocpvse184940 <hocpvse184940@fpt.edu.vn>
2025-11-10 16:44:54 +0700

    [UPDATE] add redis tech

--- LabManagementBackend/LabManagement.API/Program.cs
+++ LabManagementBackend/LabManagement.API/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace LabManagement.API
 {
@@ -32,11 +33,15 @@ namespace LabManagement.API
             // Add DbContext
             builder.Services.AddDbContext<LabManagementDbContext>(options =>
                 options.UseSqlServer(connString));
-            
+
+            builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+                    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("UptashRedis")));
+
             // Add unit of work / repositories
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             
             // Add services
+            builder.Services.AddScoped<IRedisHelper, RedisHelper>();
             builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<IGoogleAuthService, GoogleAuthService>();
---
+++ LabManagementBackend/LabManagement.BLL/Implementations/RedisHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using LabManagement.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManagement.BLL.Implementations
+{
+    public class RedisHelper : IRedisHelper
+    {
+    }
+}
